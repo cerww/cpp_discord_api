cb57640a657f216a7239797eafc6e287417da7c4
cerww <calebtran1@hotmail.com>
2020-08-04 04:17:59 -0700

    changed folder structure

--- include/arrow_proxy.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-template<class Reference>
-struct arrow_proxy {
-	mutable Reference r;
-	Reference* operator->()const {
-		return &r;
-	}
-};
--- include/crtp_stuff.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-template<typename T>
-struct crtp {
-	constexpr T& self() { return static_cast<T&>(*this); }
-
-	constexpr T const& self() const { return static_cast<T const&>(*this); }
-};
